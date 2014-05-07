81d030cce6132ae96bec3356a9f3cb7f0fba83d0
Przybysz <patryk_przybysz@hotmail.se>
2014-05-07 21:11:11 +0200

    Initial Commit

---
+++ stockapp/GraphicsController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Patryk
+ */
+public class GraphicsController implements Initializable {
+
+	private Logic logic;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
+	
+	public void setLogicHandler(Logic l) {
+		logic = l;
+	}
+	
+	public Logic getLogic() {
+		return logic;
+	}
+}
---
+++ stockapp/Logic.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stockapp;
+
+import stockapp.stockreader.StockReader;
+import java.util.HashMap;
+import javafx.stage.Stage;
+import stockapp.graphics.GUI;
+import stockapp.modules.nativegraph.NativeGraph;
+import stockapp.stockreader.Stock;
+
+public class Logic {
+	private GUI gui;
+	private StockReader reader;
+	
+	private HashMap<String, Module> modules;
+	
+	public Logic(Stage stage) {
+		modules = new HashMap<>();
+		initNativeModules();
+		try {
+			gui = new GUI(stage, this);
+		} catch(Exception ex) {}
+		reader = new StockReader();
+		
+	}
+	public void loadModule(Module mod) {
+		modules.put(mod.getPath(), mod);
+	}
+	
+	public void showModule(String path) {
+		gui.openModule(getModule(path).getRoot());
+	}
+	
+	public Module getModule(String path) {
+		Module m = modules.get(path);
+		if(m == null) {
+			throw new IllegalArgumentException("There is no module with that path.");
+		}
+		return m;
+	}
+	
+	public Stock getStock(String id) {
+		throw new UnsupportedOperationException();
+	}
+	
+	private void initNativeModules(){
+		try {
+			loadModule(new NativeGraph(this));
+		} catch(Exception ex) {ex.printStackTrace();}
+	}
+}
