99fafb440fa3c029835d782b6765e1fabf2adee0
AdamThuringer <AdamThuringer@gmail.com>
2014-05-15 18:41:14 +0200

    Funkar iaf
    :)

--- Halp/NativeGraphController.java
+++ Halp/NativeGraphController.java
@@ -6,12 +6,12 @@ package stockapp.modules.nativegraph;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import stockapp.Equity;
 import stockapp.GraphicsController;
 import stockapp.Logic;
-import stockapp.Equity;
 
 /**
  * FXML Controller class
@@ -19,11 +19,23 @@ import stockapp.Equity;
  * @author Patryk
  */
 public class NativeGraphController extends GraphicsController {
-    
-    @FXML private LineChart LineChart;
-    
-    @FXML private void handleLineChart() {
-        LineChart.setTitle("Stock Monitoring, 2010");
+
+    public NativeGraphController() {
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        this.LineChart = new LineChart<Number,Number>(xAxis,yAxis);
+    }
+
+    @FXML
+    private void handleOptionsButton(ActionEvent event) {
+
+    }
+    @FXML
+    private final LineChart<Number,Number> LineChart;;
+
+    @FXML
+    private void handleLineChart(ActionEvent event) {
+
         //defining a series
         XYChart.Series series = new XYChart.Series();
         series.setName("My portfolio");
@@ -37,29 +49,27 @@ public class NativeGraphController extends GraphicsController {
         series.getData().add(new XYChart.Data(7, 22));
         series.getData().add(new XYChart.Data(8, 45));
         series.getData().add(new XYChart.Data(9, 43));
-        Scene scene = new Scene(LineChart,800,600);
-        LineChart.getData().add(series);        
+        LineChart.getData().add(series);
     }
-	
-	@FXML private void handleOptionsButton(ActionEvent event) {
-		
-	}
-		
-	@FXML private void requestOMX(ActionEvent event) {
-		checkLogic();
-		Logic logic = getLogic();
-		logic.getEquity("name-of-stock"); //The format of the stock identification will be decided later
-	}
-	
-	@FXML private void requestNASDAQ(ActionEvent event) {
-		
-	}
-	
-	@FXML private void requestYahoo(ActionEvent event) {
-		checkLogic();
-		Equity eq = getLogic().getEquity("YHOO");
-		for(String s : eq.keySet()) {
-			System.out.println("-> " + s);
-		}
-	}
+
+    @FXML
+    private void requestOMX(ActionEvent event) {
+        checkLogic();
+        Logic logic = getLogic();
+        logic.getEquity("name-of-stock"); //The format of the stock identification will be decided later
+    }
+
+    @FXML
+    private void requestNASDAQ(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void requestYahoo(ActionEvent event) {
+        checkLogic();
+        Equity eq = getLogic().getEquity("YHOO");
+        for (String s : eq.keySet()) {
+            System.out.println("-> " + s);
+        }
+    }
 }
