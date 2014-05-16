1e9e09738b5ac3549df3e8197bd2b51778f1e014
Przybysz <patryk_przybysz@hotmail.se>
2014-05-16 15:44:03 +0200

    Final

--- stockapp/Logic.java
+++ stockapp/Logic.java
@@ -2,6 +2,8 @@ package stockapp;
 
 import stockapp.stockreader.StockReader;
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 import javafx.stage.Stage;
 import stockapp.graphics.GUI;
 import stockapp.modules.mainwindow.MainWindow;
@@ -12,10 +14,13 @@ public class Logic {
 	private StockReader reader;
 	
 	private HashMap<String, Module> modules;
+	private HashMap<String, Index> indices;
 	
 	public Logic(Stage stage) {
 		modules = new HashMap<>();
+		indices = new HashMap<>();
 		initNativeModules();
+		initDefaultIndices();
 		try {
 			gui = new GUI(stage, this);
 			gui.openModule(getModule("com.southbridge.mainwindow").getRoot());
@@ -31,6 +36,7 @@ public class Logic {
 	
 	public void showModule(String path) {
 		gui.openModule(getModule(path).getRoot());
+		getModule(path).refresh();
 	}
 	
 	public Module getModule(String path) {
@@ -45,10 +51,25 @@ public class Logic {
 		return reader.getEquity(symbol, startDate, endDate);
 	}
 	
+	public Set<Entry<String, Index>> getIndices() {
+		return indices.entrySet();
+	}
+	
 	private void initNativeModules(){
 		try {
 			loadModule(new MainWindow(this));
 			loadModule(new NativeGraph(this));
 		} catch(Exception ex) {ex.printStackTrace();}
+	}
+	
+	private void initDefaultIndices() {
+		Index omx = new Index("^OMXS30");
+		omx.putEquity("ABB.ST", "ABB Ltd.");
+		omx.putEquity("ERIC-B.ST", "Ericsson B");
+		omx.putEquity("INVE-B.ST", "Investor B");
+		omx.putEquity("LUPE.ST", "Lundin Petroleum");
+		omx.putEquity("SAND.ST", "Sandvik");
+		omx.putEquity("TLSN.ST", "Telia Sonera");
+		indices.put("OMXS30", omx);
 	}
 }
--- stockapp/modules/nativegraph/NativeGraphController.java
+++ stockapp/modules/nativegraph/NativeGraphController.java
@@ -1,28 +1,52 @@
 package stockapp.modules.nativegraph;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.ResourceBundle;
 import java.util.Set;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
 import stockapp.GraphicsController;
 import stockapp.Equity;
 import stockapp.EquityDay;
+import stockapp.Index;
 
 public class NativeGraphController extends GraphicsController {
 	@FXML private Label stockPrice;
 	@FXML private Label stockName;
 	@FXML private LineChart<Number, Number> chart;
 	@FXML private TextField searchField;
+	@FXML private SplitMenuButton indexSelect;
+	@FXML private ListView<Entry<String, String>> stockList;
 	
     private String symbol;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		stockList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Entry<String, String>>(){
+			@Override
+			public void changed(ObservableValue<? extends Entry<String, String>> ov, Entry<String, String> t, Entry<String, String> t1) {
+				if(t1 != null) {
+					symbol = t1.getKey();
+					refresh();
+				}
+			}
+		});
+	}
 	
 	@FXML private void returnToMain(ActionEvent event) {
 		checkLogic();
@@ -37,10 +61,40 @@ public class NativeGraphController extends GraphicsController {
 		}
 	}
 	
+	private void showIndexEquites(Index ind) {
+		stockList.getItems().clear();
+		for(Entry<String, String> entry : ind.getEntrySet()) {
+			stockList.getItems().add(entry);
+			
+		}
+	}
+	
 	@Override
 	public void refresh() {
 		checkLogic();
 		clear();
+		
+		Set<Entry<String, Index>> indices = getLogic().getIndices();
+		for(Entry<String, Index> entry : indices) {
+			MenuItem item = new MenuItem();
+			item.setText(entry.getKey());
+			final Index ind = entry.getValue();
+			item.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent t) {
+					showIndexEquites(ind);
+				}
+			});
+			indexSelect.getItems().add(item);
+		}
+		
+		updateChart();
+	}
+	
+	private void updateChart() {
+		if(symbol == null) {
+			return;
+		}
 		Equity eq = getLogic().getEquity(symbol, "2014-01-01", "2014-05-15");
 		stockName.setText(eq.getVariable("Name"));
 		stockPrice.setText(eq.getVariable("BidRealtime"));
@@ -62,6 +116,8 @@ public class NativeGraphController extends GraphicsController {
 	
 	private void clear() {
 		chart.getData().clear();
+		indexSelect.getItems().clear();
+		stockList.getSelectionModel().clearSelection();
 	}
 	
 	private ArrayList<Entry<String, EquityDay>> sortDates(Set<Entry<String, EquityDay>> set) {
